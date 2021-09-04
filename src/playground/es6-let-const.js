53f00014eb317ac4a90679155e21231966c36dac
Adrian Morley <adrianmorley95@gmail.com>
2021-09-04 14:53:51 +0100

    Initial commit.

---
+++ src/playground/es6-let-const.js
@@ -0,0 +1,10 @@
+var nameVar = 'Andrew';
+console.log('nameVar', nameVar);
+
+//cant reuse nameLet for variable name
+let nameLet = 'Jen';
+console.log('nameLet', nameLet);
+
+//cant redefine const value
+const nameConst = 'Frank';
+console.log('nameConst', nameConst);
